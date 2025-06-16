0876aeda4233774b2e8527053d53567fd32fca4e
SAMANPREET BAWA <157168862+saman7568@users.noreply.github.com>
2025-06-16 18:40:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile nav
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
